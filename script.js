2979f697dadf428db45301de47c9047cbceef224
Frank Buono <fbuonojr@fordham.edu>
2020-10-21 19:51:09 -0400

    randColor()

--- script.js
+++ script.js
@@ -19,5 +19,27 @@ $(document).ready(function(){
     ];
 
     //this function sets a random color and a random set of colors
-    function
+    function randColor() {
+        //sets random word
+        targetColor = colors[Math.floor(Math.random() * colors.length)];
+        $("#rand-target-color").text(targetColor);
+
+        //sets random word color
+        targetColorOfText = colors[Math.floor(Math.random() * colors.length)];
+        $("#rand-target-color").css("color", targetColorOfText);
+    }
+
+    //this function creates the logic for the game
+    function createColorPicker() {
+
+        //color array shuffler
+        colors.sort(function() {
+            return 0.5 - Math.random();
+        });
+
+        //push to text color array
+        for (var i = 0; i < colors.length; i++){
+            colorsOfText.push(colors[i]);
+        }
+    }
 });
