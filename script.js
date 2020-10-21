b0a985874bac01a14a5190c1b6530a5d04b406e9
Frank Buono <fbuonojr@fordham.edu>
2020-10-21 19:41:12 -0400

    added global variables and arrays

---
+++ script.js
@@ -0,0 +1,23 @@
+//page cannot be manipulated until document is ready
+$(document).ready(function(){
+
+    var colorsOfText = [];
+
+    var targetColor;
+
+    var targetColorOfText;
+
+    var userAnswer;
+
+    var colors = [
+        "brown",
+        "blue",
+        "magenta",
+        "teal",
+        "coral",
+        "black"
+    ];
+
+    //this function sets a random color and a random set of colors
+    function
+});
